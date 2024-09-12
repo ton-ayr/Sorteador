ddf628d9c34f3a8259e92b4ea20e4f19821edecc
ton-ayr <ayrtoncbarreira@gmail.com>
2024-09-12 17:27:59 -0300

    Logica para sortear nomes e edição do html

---
+++ app.js
@@ -0,0 +1,37 @@
+let sorteioRealizado = false;
+
+function sortearNomes() {
+    let nomes = document.getElementById('de').value;
+    let listaNomes = nomes.split(',').map(nome => nome.trim());
+
+    if (listaNomes.length < 2) {
+        alert('Você precisa informar pelo menos dois nomes separados por vírgula.');
+        return;
+    }
+
+    let nomeSorteado = listaNomes[Math.floor(Math.random() * listaNomes.length)];
+
+    let textoResultado = document.getElementById('resultado');
+    textoResultado.innerHTML = `<label class="texto__paragrafo">Nome sorteado: ${nomeSorteado}.</label>`;
+
+    sorteioRealizado = true;
+    trocarStatusBotao();
+}
+
+function trocarStatusBotao() {
+    let botao = document.getElementById('btn-reiniciar');
+    if (sorteioRealizado) {
+        botao.classList.remove('container__botao-desabilitado');
+        botao.classList.add('container__botao');
+    } else {
+        botao.classList.remove('container__botao');
+        botao.classList.add('container__botao-desabilitado');
+    }
+}
+
+function reiniciar() {
+    document.getElementById('de').value = '';
+    document.getElementById('resultado').innerHTML = '<label class="texto__paragrafo">Nomes sorteados: nenhum até agora</label>';
+    sorteioRealizado = false;
+    trocarStatusBotao();
+}
