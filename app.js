8eae0ea06217cce8214c349d85e762d8b09a3cb2
ton-ayr <ayrtoncbarreira@gmail.com>
2024-09-16 20:33:55 -0300

    Adicionando comentarios

--- app.js
+++ app.js
@@ -1,44 +1,58 @@
+// Variável para controlar se o sorteio já foi realizado
 let sorteioRealizado = false;
+// Array para armazenar os nomes informados
 let listaNomes = [];
 
+// Função para realizar o sorteio
 function sortearNomes() {
+    // Obtém os nomes informados
     let nomesInput = document.getElementById('de').value;
     listaNomes = nomesInput.split(',').map(nome => nome.trim());
 
+    // Verifica se há pelo menos 2 nomes informados
     if (listaNomes.length < 2) {
         alert('Você precisa informar pelo menos dois nomes separados por vírgula.');
         return;
     }
 
+    // Obtém a quantidade informada
     let quantidade = parseInt(document.getElementById('quantidade').value);
 
+    // Verifica se a quantidade é válida
     if (isNaN(quantidade) || quantidade <= 0) {
         alert('A quantidade deve ser maior que 0.');
         return;
     }
 
+    // Verifica se a quantidade é maior que o número de nomes
     if (quantidade > listaNomes.length) {
         alert('A quantidade não pode ser maior que o número de nomes informados.');
         return;
     }
 
+    // Array para armazenar os nomes sorteados
     let nomesSorteados = [];
     while (nomesSorteados.length < quantidade) {
+        // Faz o sorteio
         let indiceSorteado = Math.floor(Math.random() * listaNomes.length);
         let nomeSorteado = listaNomes[indiceSorteado];
 
+        // Verifica se o nome já foi sorteado
         if (!nomesSorteados.includes(nomeSorteado)) {
             nomesSorteados.push(nomeSorteado);
         }
     }
 
+    // Exibe os nomes sorteados
     let textoResultado = document.getElementById('resultado');
     textoResultado.innerHTML = `<label class="texto__paragrafo">Nomes sorteados: ${nomesSorteados.join(', ')}.</label>`;
 
+    // Atualiza o status do botao reiniciar
     sorteioRealizado = true;
     trocarStatusBotao();
 }
 
+// Função para trocar o status do botão de reiniciar
 function trocarStatusBotao() {
     let botao = document.getElementById('btn-reiniciar');
     if (sorteioRealizado) {
@@ -50,6 +64,7 @@ function trocarStatusBotao() {
     }
 }
 
+// Função para reiniciar o sorteio
 function reiniciar() {
     document.getElementById('de').value = '';
     document.getElementById('quantidade').value = '';
